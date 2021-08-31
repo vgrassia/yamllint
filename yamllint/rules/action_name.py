e513dacc424f2d75dc9b058169be2a0b8649829d
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2021-08-31 11:10:38 -0400

    Update action rules

--- yamllint/rules/action_name.py
+++ yamllint/rules/action_name.py
@@ -14,126 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""
-Use this rule to control the use of flow mappings or number of spaces inside
-braces (``{`` and ``}``).
-
-.. rubric:: Options
-
-* ``forbid`` is used to forbid the use of flow mappings which are denoted by
-  surrounding braces (``{`` and ``}``). Use ``true`` to forbid the use of flow
-  mappings completely. Use ``non-empty`` to forbid the use of all flow
-  mappings except for empty ones.
-* ``min-spaces-inside`` defines the minimal number of spaces required inside
-  braces.
-* ``max-spaces-inside`` defines the maximal number of spaces allowed inside
-  braces.
-* ``min-spaces-inside-empty`` defines the minimal number of spaces required
-  inside empty braces.
-* ``max-spaces-inside-empty`` defines the maximal number of spaces allowed
-  inside empty braces.
-
-.. rubric:: Default values (when enabled)
-
-.. code-block:: yaml
-
- rules:
-   braces:
-     forbid: false
-     min-spaces-inside: 0
-     max-spaces-inside: 0
-     min-spaces-inside-empty: -1
-     max-spaces-inside-empty: -1
-
-.. rubric:: Examples
-
-# . With ``braces: {forbid: true}``
-
-   the following code snippet would **PASS**:
-   ::
-
-    object:
-      key1: 4
-      key2: 8
-
-   the following code snippet would **FAIL**:
-   ::
-
-    object: { key1: 4, key2: 8 }
-
-# . With ``braces: {forbid: non-empty}``
-
-   the following code snippet would **PASS**:
-   ::
-
-    object: {}
-
-   the following code snippet would **FAIL**:
-   ::
-
-    object: { key1: 4, key2: 8 }
-
-# . With ``braces: {min-spaces-inside: 0, max-spaces-inside: 0}``
-
-   the following code snippet would **PASS**:
-   ::
-
-    object: {key1: 4, key2: 8}
-
-   the following code snippet would **FAIL**:
-   ::
-
-    object: { key1: 4, key2: 8 }
-
-# . With ``braces: {min-spaces-inside: 1, max-spaces-inside: 3}``
-
-   the following code snippet would **PASS**:
-   ::
-
-    object: { key1: 4, key2: 8 }
-
-   the following code snippet would **PASS**:
-   ::
-
-    object: { key1: 4, key2: 8   }
-
-   the following code snippet would **FAIL**:
-   ::
-
-    object: {    key1: 4, key2: 8   }
-
-   the following code snippet would **FAIL**:
-   ::
-
-    object: {key1: 4, key2: 8 }
-
-# . With ``braces: {min-spaces-inside-empty: 0, max-spaces-inside-empty: 0}``
-
-   the following code snippet would **PASS**:
-   ::
-
-    object: {}
-
-   the following code snippet would **FAIL**:
-   ::
-
-    object: { }
-
-# . With ``braces: {min-spaces-inside-empty: 1, max-spaces-inside-empty: -1}``
-
-   the following code snippet would **PASS**:
-   ::
-
-    object: {         }
-
-   the following code snippet would **FAIL**:
-   ::
-
-    object: {}
-"""
-
-
 import yaml
+from yaml.tokens import BlockSequenceStartToken, FlowSequenceStartToken, ScalarToken
 
 from yamllint.linter import LintProblem
 # from yamllint.rules.common import spaces_after, spaces_before
@@ -143,38 +25,84 @@ ID = 'action-name'
 TYPE = 'token'
 CONF = {}
 DEFAULT = {}
-# CONF = {'forbid': (bool, 'non-empty'),
-#         'min-spaces-inside': int,
-#         'max-spaces-inside': int,
-#         'min-spaces-inside-empty': int,
-#         'max-spaces-inside-empty': int}
-# DEFAULT = {'forbid': False,
-#            'min-spaces-inside': 0,
-#            'max-spaces-inside': 0,
-#            'min-spaces-inside-empty': -1,
-#            'max-spaces-inside-empty': -1}
+
+MAP, SEQ = range(2)
+
+
+class Parent(object):
+    def __init__(self, type):
+        self.type = type
+        self.keys = []
+
+    def __str__(self):
+        if self.type == 0:
+            type = "MAP"
+        elif self.type == 1:
+            type = "SEQ"
+        return "Type: " + type + " | Keys: " + str(self.keys)
+
+    def get(self, key):
+        result = []
+
+        for x in self.keys:
+            if x.value == key:
+                result.append(x)
+
+        return result
 
 
 def check(conf, token, prev, next, nextnext, context):
-    if isinstance(token, yaml.DocumentStartToken):
-        context['workflow-name'] = False
+    # If we don't have a stack, create an empty list to hold one.
+    if 'stack' not in context:
+        context['stack'] = []
 
-    if isinstance(prev, yaml.DocumentStartToken):
-        if isinstance(token, yaml.BlockMappingStartToken):
-            if isinstance(next, yaml.KeyToken):
-                if isinstance(nextnext, yaml.ScalarToken) and nextnext.value == 'name':
-                    context['workflow-name'] = True
+    # If we are starting a MAP Block/Flow append it to the stack.
+    if isinstance(token, (yaml.BlockMappingStartToken,
+                          yaml.FlowMappingStartToken)):
+        context['stack'].append(Parent(MAP))
 
-    if isinstance(token, (yaml.StreamEndToken, yaml.DocumentEndToken)):
-        if not context['workflow-name'] == True:
-            yield LintProblem(token.start_mark.line + 1,
-                              token.end_mark.column + 1,
-                              'workflow missing name key')
+    # If we are starting a SEQ Block/Flow append it to the stack.
+    elif isinstance(token, (yaml.BlockSequenceStartToken,
+                            yaml.FlowSequenceStartToken)):
+        context['stack'].append(Parent(SEQ))
 
-    if isinstance(next, yaml.ScalarToken) and next.value == 'name':
-        print("-----")
-        print(prev)
-        print(token)
-        print(next)
-        print(nextnext)
-        print("-----")
+    # If we have a Key token with a Scalar value token next,
+    # append it to the keys of the last MAP/SEQ in the stack.
+    elif (isinstance(token, yaml.KeyToken) and
+          isinstance(next, yaml.ScalarToken)):
+        context['stack'][-1].keys.append(next)
+
+    # If we are ending a Block/FlowMap/FlowSeq then run some checks
+    # against the stack.
+    elif isinstance(token, (yaml.BlockEndToken,
+                            yaml.FlowMappingEndToken,
+                            yaml.FlowSequenceEndToken)):
+        # Check to make sure 'name' key exists for workflow.
+        if len(context['stack']) == 1:
+            workflow_name = context['stack'][0].get('name')
+            if len(workflow_name) != 1:
+                yield LintProblem(context['stack'][0].keys[0].start_mark.line + 1,
+                                  context['stack'][0].keys[0].end_mark.column + 1,
+                                  'missing name key for workflow')
+
+        # Check to make sure 'name' key exists for jobs.
+        if len(context['stack']) == 3:
+            jobs = context['stack'][0].get('jobs')
+            if len(jobs) == 1:
+                job_names = context['stack'][2].get('name')
+                if len(job_names) != 1:
+                    yield LintProblem(context['stack'][1].keys[-1].start_mark.line + 1,
+                                      context['stack'][1].keys[-1].end_mark.column + 1,
+                                      'missing name key for job')
+
+        # Check to make sure 'name' key exists for steps.
+        if len(context['stack']) == 5:
+            steps = context['stack'][2].get('steps')
+            if len(steps) == 1:
+                step_names = context['stack'][4].get('name')
+                if len(step_names) != 1:
+                    yield LintProblem(context['stack'][4].keys[0].start_mark.line + 1,
+                                      context['stack'][4].keys[0].end_mark.column + 1,
+                                      'missing name key for step')
+
+        context['stack'].pop()
